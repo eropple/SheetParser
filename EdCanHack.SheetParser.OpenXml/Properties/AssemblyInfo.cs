e2b852b3d29e212e555118eca4ce64a621444698
Ed Ropple <ed@edropple.com>
2015-03-18 01:23:11 -0400

    fixed OpenXML assembly attributes for nuget

--- EdCanHack.SheetParser.OpenXml/Properties/AssemblyInfo.cs
+++ EdCanHack.SheetParser.OpenXml/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EdCanHack.SheetParser.OpenXml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Provides SheetParser the ability to read OpenXML spreadsheets.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ed Ropple (ed+sheetparser@edropple.com)")]
 [assembly: AssemblyProduct("EdCanHack.SheetParser.OpenXml")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015 Ed Ropple")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
